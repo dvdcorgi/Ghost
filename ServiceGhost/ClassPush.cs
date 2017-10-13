46725b84799808b05f31efea3d873d67b6b077b6
dadat <dvdbarredo@gmail.com>
2017-10-13 16:13:59 +0800

    Commit class for push

---
+++ ServiceGhost/ClassPush.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceGhost
+{
+    public class ClassPush
+    {
+        public void PushItem()
+        {
+            var item = "Push this item.";
+            Console.WriteLine();
+        }
+    }
+}
